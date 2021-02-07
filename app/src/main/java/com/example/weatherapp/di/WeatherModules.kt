65e16c8f424a34498232676de8b9a2c079f6e606
Mohamed Saleh <mohamed_andela@medable.com>
2021-02-08 00:31:27 +0200

    add network dependecies
    add the dependecies for network calls

--- app/src/main/java/com/example/weatherapp/di/WeatherModules.kt
+++ app/src/main/java/com/example/weatherapp/di/WeatherModules.kt
@@ -1,10 +1,39 @@
 package com.example.weatherapp.di
 
 import android.app.Activity
+import com.example.weatherapp.BuildConfig
 import com.example.weatherapp.ui.MainViewModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private fun provideOkHttpClient() = if (BuildConfig.DEBUG) {
+    val loggingInterceptor = HttpLoggingInterceptor()
+    loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+    OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+} else {
+    OkHttpClient.Builder().build()
+}
+
+private fun provideRetrofit(okHttpClient: OkHttpClient, baseUrl: String): Retrofit {
+    return Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .baseUrl(baseUrl)
+        .build()
+}
 
 val applicationModule = module {
     viewModel { (activity: Activity) -> MainViewModel(activity) }
+
+    single {
+        provideOkHttpClient()
+    }
+
+    single { provideRetrofit(get(), BuildConfig.BASE_URL) }
 }
